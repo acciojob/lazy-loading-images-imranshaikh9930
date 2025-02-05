e996ad9a204c712a9e395b3cc11d5d2ff4f1a658
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-02-05 12:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+
+
+// code of lazy loading
+// detect viewPort of image 
+
+
+document.addEventListener("DOMContentLoaded",function(){
+
+  const lazyImages = document.querySelectorAll("img.lazy");
+
+  const imageObserver = new IntersectionObserver((entries,observer)=>{
+    entries.forEach((entry)=> {
+      console.log(entry)
+      if(entry.isIntersecting){
+        const img = entry.target;
+        img.src= img.dataset.src ;
+        img.classList.remove("lazy");
+        observer.unobserve(img)
+      }
+    });
+  })
+
+  lazyImages.forEach((img)=>{
+    imageObserver.observe(img);
+  })
+
+  // console.log(imageObserver);
+})
